f1c8dc63cfd8719ef77d118ce5c48fe92dd11053
omarMohamedAbdo <omarknighto@gmail.com>
2020-05-10 06:07:42 +0200

    Get Popular Categories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -45,4 +45,26 @@ router.delete('/:id', adminAuth, async (req, res, next)=>{
     }   
 });
 
+//Get Popular Categories
+router.get('/top_categories', (req, res, next)=>{
+    try {
+        Category.find({}).populate('books').limit(5).exec(function(err, docs) {
+            docs.sort(function compare(a, b){
+                let c = 0;    
+                if (a.books.rate > b.books.rate) {
+                     c = 1;
+                } else if (b.books.rate > a.books.rate) {
+                     c = -1;
+                }    
+                return c;
+            });
+            // return or response with docs
+            if (err) return res.send(err);
+            res.status(200).json(docs);
+        });   
+    } catch (error) {
+        next(error);
+    }   
+});
+
 module.exports = router;
