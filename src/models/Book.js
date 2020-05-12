f0c499011baec263825091ee98daded4244c58f2
Islam <iabdelhamid3@gmail.com>
2020-05-12 11:12:31 +0200

    fix book relations

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Category = require('./Category');
+const Author = require('./Author');
 
 const bookSchema = new mongoose.Schema(
   {
@@ -25,7 +27,7 @@ const bookSchema = new mongoose.Schema(
     },
     rate: {
       type: mongoose.Decimal128,
-      required:false
+      required: false,
     },
     reviews: [
       {
@@ -33,18 +35,41 @@ const bookSchema = new mongoose.Schema(
         comment: String,
         userId: {
           type: mongoose.Schema.Types.ObjectId,
-          ref: 'User'
+          ref: 'User',
         },
       },
       {
-        timestamps: true
-      }
-    ]
+        timestamps: true,
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+bookSchema.post('save', async function () {
+  const book = this;
+  try {
+    await Author.findByIdAndUpdate(book.author, {
+      $push: { books: book.id },
+    });
+
+    await Category.findByIdAndUpdate(book.category, {
+      $push: { books: book.id },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+// bookSchema.methods.rate = async function (user, rate, comment) {
+//   // if (rate > 5 || rate < 0) throw Error('invalid rate value');
+//   // const book = this;
+//   // if(book.)
+//   // try {
+//   // }
+// };
 
 const Book = mongoose.model('Book', bookSchema);
 
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,127 +1,132 @@
 const { Router } = require('express');
 
-const Book = require('../models/Book')
-const User = require('../models/User')
+const Book = require('../models/Book');
+const User = require('../models/User');
 const userAuth = require('../middleware/userAuth');
 const adminAuth = require('../middleware/adminAuth');
 
 const router = new Router();
 
 // Normal CRUD operations //
-router.get('', userAuth, async (req, res, next)=>{
-    try {
-        const pagination = req.query.pagination
-        ? parseInt(req.query.pagination)
-        : 15;
-        const page = req.query.page ? parseInt(req.query.page) :1;
-        book = await Book.find({}) 
-        .skip((page-1) * pagination)
-        .limit(pagination)
-        .populate('category')
-        .populate('author')
-        .populate('reviews');
-        res.status(200).json(book);
-    } catch (error) {
-        next(error)
-    }
+router.get('/', userAuth, async (req, res, next) => {
+  try {
+    const pagination = req.query.pagination
+      ? parseInt(req.query.pagination)
+      : 10;
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    books = await Book.find({})
+      .skip((page - 1) * pagination)
+      .limit(pagination);
+    // .populate('category')
+    // .populate('author')
+    // .populate('reviews');
+    res.status(200).json(books);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //Get Popular Books
-router.get('/top_books', async (req, res, next)=>{
-    try {
-        book = await Book.find({}).sort({rate: -1}).limit(5);
-        res.status(200).json(book);
-    } catch (error) {
-        next(error)
-    }
+router.get('/top_books', async (req, res, next) => {
+  try {
+    book = await Book.find({}).sort({ rate: -1 }).limit(5);
+    res.status(200).json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', userAuth, async (req, res, next)=>{
-    try {
-        book = await Book.findById(req.params.id)
-        .populate('category')
-        .populate('author');
-        res.status(200).json(book);
-    } catch (error) {
-        next(error)
-    }
+router.get('/:id', userAuth, async (req, res, next) => {
+  try {
+    book = await Book.findById(req.params.id)
+      .populate('category')
+      .populate('author');
+    res.status(200).json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', adminAuth, async (req, res, next)=>{
-    let { body } = req;
-    let book = new Book(body);
-    try {
-        await book.save();
-        res.status(201).json(book);
-    } catch (error) {
-        next(error)
-    }
+router.post('/', adminAuth, async (req, res, next) => {
+  let { body } = req;
+  let book = new Book(body);
+  try {
+    await book.save();
+    res.status(201).json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.patch('/:id', adminAuth, async (req, res, next)=>{
-    let { body } = req;
-    try {
-        let book = await Book.findByIdAndUpdate(req.params.id, {$set: body}, {new: true});
-        res.status(200).json(book);
-    } catch (error) {
-        next(error)
-    }
+router.patch('/:id', adminAuth, async (req, res, next) => {
+  let { body } = req;
+  try {
+    let book = await Book.findByIdAndUpdate(
+      req.params.id,
+      { $set: body },
+      { new: true }
+    );
+    res.status(200).json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', adminAuth, async (req, res, next)=>{
-    try {
-        await Book.findByIdAndDelete(req.params.id);
-        res.status(200).send("Book has been deleted successfully");
-        next();     
-    } catch (error) {
-        next(error);
-    }   
+router.delete('/:id', adminAuth, async (req, res, next) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.status(200).send('Book has been deleted successfully');
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
-
-
 
 // reviews end points //
 
 // create review //
-router.post('/:id', userAuth, async (req, res, next)=>{
-    let { body: {rate, comment} } = req;
-    let review = {rate, comment, userId: req.user.id}
-    try {
-        user = req.user
-        book = await Book.findById(req.params.id);
-        userBook = user.books.filter(b => b.id == book.id)
-        if(userBook.status != 1){
-            res.status(403).send("You can't review the book unless you read it!");
-            return
-        }
-        book.reviews.push(review)
-        await book.save();
-        user.reviews.push(review)
-        await user.save()
-        res.status(201).json(book);
-    } catch (error) {
-        next(error)
+router.post('/:id', userAuth, async (req, res, next) => {
+  let {
+    body: { rate, comment },
+  } = req;
+  let review = { rate, comment, userId: req.user.id };
+  try {
+    user = req.user;
+    book = await Book.findById(req.params.id);
+    userBook = user.books.filter((b) => b.id == book.id);
+    if (userBook.status != 1) {
+      res.status(403).send("You can't review the book unless you read it!");
+      return;
     }
+    book.reviews.push(review);
+    await book.save();
+    user.reviews.push(review);
+    await user.save();
+    res.status(201).json(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // change book status //
-router.patch('/:id/change-status', userAuth, async (req, res, next)=>{
-    let { body: {status} } = req;
-    try {
-        User.findOne({_id: req.user.id}).then(doc => {
-            book = doc.books.id({bookId: req.params.id});
-            if(book){
-                book["status"] = status;
-            }
-            else{
-                doc.books.push({bookId: req.params.id, status})
-            }
-            doc.save();
-        })
-        res.status(200).send("book status has been changed successfully");
-    } catch (error) {
-        next(error)
-    }
+router.patch('/:id/change-status', userAuth, async (req, res, next) => {
+  let {
+    body: { status },
+  } = req;
+  try {
+    User.findOne({ _id: req.user.id }).then((doc) => {
+      book = doc.books.id({ bookId: req.params.id });
+      if (book) {
+        book['status'] = status;
+      } else {
+        doc.books.push({ bookId: req.params.id, status });
+      }
+      doc.save();
+    });
+    res.status(200).send('book status has been changed successfully');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
