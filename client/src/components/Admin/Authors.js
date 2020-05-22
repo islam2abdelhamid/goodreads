a9f6db3d4bdd86a1497a559bc3b7d315952f8a48
Mohamed Elshafeay <mohamedelshafeay@gmail.com>
2020-05-23 00:58:04 +0200

    Adding Books & Authors & Categories pages for Admin

--- client/src/components/Admin/Authors.js
+++ client/src/components/Admin/Authors.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import axios from '../../axios';
+import requireAdmin from '../../hocs/requireAdmin'
 
 const Authors = () => {
-  return (
-        <h1>Admin Authors</h1>
+    const [authors, setAuthors] = useState([]);
+    const token = localStorage.getItem('goodReadsToken');
+    let index = 0
+    useEffect(() => {
+        axios
+          .get('/authors', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((result) => {
+            setAuthors(authors.concat(result.data))
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, [])
+
+    return (
+      <div className="col-8 m-auto">
+        <h2 className="pink-text">Authors</h2>
+        <table className="table table-bordered justify-content-center text-center ">
+            <thead>
+                  <tr className="thead-dark">
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Image</th>
+                        <th>Date Of Birth</th>
+                        <th colSpan="2">Actions</th>
+                  </tr>
+            </thead>
+        <tbody>
+        {authors.map((author)=>(
+            <tr key={author._id}>
+                <td className='align-middle text-light'>{index++}</td>
+                <td className='align-middle editable text-light' data-id={ author.id }>{author.firstName}</td>
+                <td className='align-middle editable text-light' data-id={ author.id }>{author.lastName}</td>
+                <td className='align-middle'><img class='img-thumbnail rounded table__img' src="https://picsum.photos/200/300"/></td>
+                <td className='align-middle text-light'>{new Date(`${author.dateOfBirth}`).toDateString().slice(3,)}</td>
+                <td className='align-middle text-light' data-id={ author.id }><i className="fa fa-edit"></i></td>
+                <td className='align-middle text-light' data-id={ author.id }><i className="fa fa-trash"></i></td>
+            </tr>
+            )
+        )}
+        </tbody>
+    </table>
+    </div>
   );
 };
-export default Authors;
+export default requireAdmin(Authors);
--- client/src/components/Admin/Header.js
+++ client/src/components/Admin/Header.js
@@ -8,7 +8,7 @@ const AdminHeader = () => {
   return (
     <div className='sign section--bg' data-bg='img/section/section.jpg'>
       <nav className="navbar navbar-dark bg-dark col-3 m-auto">
-        <p class="navbar-brand" style={{marginTop: "-2rem"}} href="">Admin Pages</p>
+        <p className="navbar-brand" style={{marginTop: "-2rem"}} href="">Admin Pages</p>
         <Link className="header__nav-link" to="/admin/categories/">Categories</Link>
         <Link className="header__nav-link" to="/admin/books/">Books</Link>
         <Link className="header__nav-link" to="/admin/authors/">Authors</Link>
