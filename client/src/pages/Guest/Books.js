0c4e5e9556f3cd187a7432969476e8146e43e367
nahlaahmed97 <nahla.elaskry97@gmail.com>
2020-05-24 01:04:50 +0200

    popular categories and authors in guest home

--- client/src/pages/Guest/Books.js
+++ client/src/pages/Guest/Books.js
@@ -6,8 +6,12 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 import axios from '../../axios';
 import requireGuest from '../../hocs/requireGuest';
 
+
 function Books() {
   const [books, setBooks] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [authors, setAuthors] = useState([]);
+
 
   useEffect(() => {
     axios
@@ -19,6 +23,33 @@ function Books() {
 
       .then(res => {
         setBooks(res.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/categories/top_categories', {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('goodReadsToken'),
+        },
+      })
+
+      .then(res => {
+        setCategories(res.data);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/authors/top_authors', {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('goodReadsToken'),
+        },
+      })
+
+      .then(res => {
+        setAuthors(res.data);
       });
   }, []);
 
@@ -37,8 +68,8 @@ function Books() {
               <OwlCarousel className='owl-theme' margin={10} items={4} nav>
                 {books.map(item => (
                   <div className='item' key={item._id}>
-                    <div className='card card--big'>
-                      <div className='card__cover'>
+                    <div className='card card--big'style={{backgroundColor:"#2b2b31" }} >
+                      <div className='card__cover' >
                         <img src='assets/img/covers/cover.jpg' alt='' />
                         <a href='#' className='card__play'>
                           <i className='icon ion-ios-eye'></i>
@@ -46,8 +77,8 @@ function Books() {
                       </div>
 
                       <div className='card__content'>
-                        <h3 className='card__title'>
-                          <a style={{ color: 'black' }} href='#'>
+                        <h3 className='card__title'  >
+                          <a  href='#'>
                             {item.name}
                           </a>
                         </h3>
@@ -68,7 +99,47 @@ function Books() {
           </div>
         </div>
       </div>
-    </section>
+      <section class="section section--bg" data-bg="img/section/section.jpg">
+		<div class="container">
+			<div class="row">
+
+      <div class="col-6">
+					<h2 class="section__title">Popular Authors</h2>
+          {authors.map(item => (
+					<ul class="author__list">
+						<li><a href="#">{item.firstName +" "+ item.lastName}</a></li>
+					</ul>
+           ))}
+				</div>
+
+				<div class="col-6">
+					<h2 class="section__title">Popular Categories</h2>
+          {categories.map(item => (
+					<ul class="author__list">
+						<li><a href="#">{item.name}</a></li>
+					</ul>
+           ))}
+				</div>
+         
+			</div>
+		</div>
+    <footer class="footer">
+		<div class="container">
+			<div class="row">
+				
+				<div class="col-12">
+					<div class="footer__copyright">
+
+					
+					</div>
+				</div>
+			
+			</div>
+		</div>
+	</footer>
+	</section>
+    </section> 
+    
   );
 }
 
